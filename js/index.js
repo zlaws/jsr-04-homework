d7dda9a2121b1296b44fae9502d62be82db15f15
Sonyl Nagale <sonyl.nagale@nbcuni.com>
2017-12-17 10:51:16 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var genders = ["male", "female"];
+var names = ["Doc","Doonesbury","Periwinkle","Chipper","Elmer","Dolce","Babushka","Grover","Taxi","Rosebud","Ouija","Bossman","Truffles","SixToes","Sadie","Seuss","VanGogh","Bunk","SittingBull","Heidi"];
+var dogs = [];
+
+function adoptHandler(/* what paremeters do we need? */) {
+  // let's add our code here to handle adopting a dog
+}
+
+document.querySelector("#selectDog").addEventListener("submit", adoptHandler);
+
+document.querySelector("#addmore").addEventListener("click", adoptHandler);
+
+function adoptDog(/* what paremeters do we need? */) {
+  // show the 'addmore' and 'done' and console.log a message that talks about the dog.
+  // add the dog message to the array
+}
+
+function finishHandler() {
+  // let's console log all our dogs
+}
+
+document.querySelector("#done").addEventListener("click", finishHandler);
